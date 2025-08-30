737761639792bc983f586df380c2b9f9da412677
JoshuaBidjou123 <kristen.bidjou@gmail.com>
2025-08-30 12:15:38 -0400

    Commit

---
+++ Functions.py
@@ -0,0 +1,11 @@
+def intro(name):
+    print("Hello, Good morning! I am", name)
+
+user_name = input("Enter your name")
+intro (user_name)
+
+def intro(age):
+    print("Hello, Good morning! I am", age)
+    
+user_age = input("Enter your age")
+intro(user_age)
